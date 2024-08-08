021c05e1701d9d50bf8b6f2b17c574303d681dd2
Swetha <144270599+SSSwetha25@users.noreply.github.com>
2024-08-08 19:29:04 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -17,17 +17,14 @@ function calculate() {
     try {
         let expression = display.value;
 
-        // Handle percentages
         expression = expression.replace(/(\d+(\.\d+)?)(%)(\d+(\.\d+)?)/g, (match, percentNum, _, __, num) => {
             return `(${percentNum} / 100) * ${num}`;
         });
 
         expression = expression.replace(/(\d+(\.\d+)?)(%)/g, (match, num) => {
-            // If the percentage is alone or at the end, handle it
             return `(${num} / 100)`;
         });
 
-        // Evaluate the expression
         display.value = eval(expression);
     } catch (error) {
         display.value = 'Error';
